5cd167d2bab519ac661b4e6e0bf0af626713977c
OrangeXC <842012622@qq.com>
2019-06-18 09:21:28 +0800

    Upgrade packages

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,33 @@
 module.exports = {
   roots: ['__tests__'],
-  moduleFileExtensions: ['js', 'json', 'vue'],
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'vue'
+  ],
   transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest'
   },
+  transformIgnorePatterns: [
+    '/node_modules/'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+  ],
   testURL: 'http://localhost/',
-  resetModules: true,
-  clearMocks: true,
-  verbose: true,
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ],
   collectCoverage: true,
   coverageReporters: ['text'],
   collectCoverageFrom: [
