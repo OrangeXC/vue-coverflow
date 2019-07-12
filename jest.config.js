993ca34844088b29b7ad7205ff42b074f815c215
OrangeXC <842012622@qq.com>
2019-07-12 09:05:57 +0800

    Fixed jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -20,9 +20,6 @@ module.exports = {
   snapshotSerializers: [
     'jest-serializer-vue'
   ],
-  testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-  ],
   testURL: 'http://localhost/',
   watchPlugins: [
     'jest-watch-typeahead/filename',
